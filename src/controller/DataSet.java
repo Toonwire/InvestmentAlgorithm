b03483db7dd764e0b924579a9d3f12e644a25c4e
Toonwire <Toonwire@hotmail.com>
2016-03-18 16:54:53 +0100

    Cleaned up main method and delegeted work to smaller methods. Allowed for all stocks to be executed in the same run with correct resets

--- src/controller/DataSet.java
+++ src/controller/DataSet.java
@@ -8,15 +8,21 @@ public class DataSet {
 	public double getBalance() {
 		return balance;
 	}
+	
 	public void setBalance(double balance) {
 		this.balance = balance;
 	}
+	
 	public boolean hasStock() {
 		return hasStock;
 	}
+	
 	public void setHasStock(boolean hasStock) {
 		this.hasStock = hasStock;
 	}
 	
-	
+	public void reset() {
+		this.hasStock = false;
+		this.balance = 0.0;
+	}	
 }
